f2ae5c0a4bee96daeadbb1b0a7d32e07ae37ea00
Tanuja Shinde <tanuja.shinde@seagate.com>
2022-09-22 00:02:01 +0530

    added users
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,16 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+    {
+        firstName: "Tanuja",
+        lastName: "Shinde",
+        age: 23
+    }
+]
+
 router.get('/', (req, res) => {
+    console.log(users)
     res.send("Hello!!")
 });
 
