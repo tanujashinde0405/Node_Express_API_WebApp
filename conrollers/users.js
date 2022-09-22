bcef418b53843d90d1708fbc99a22ce2e36907dc
Tanuja Shinde <tanuja.shinde@seagate.com>
2022-09-22 10:37:58 +0530

    added controllers
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

---
+++ conrollers/users.js
@@ -0,0 +1,40 @@
+
+import { v4 as uuidv4} from 'uuid';
+let users = []
+
+export const createUser = (req, res) => {
+    const user = req.body;
+    const userWithID = {...user, id: uuidv4()}
+    users.push(userWithID);
+    res.send(`User with the name ${user.firstName} added to the database`);
+}
+
+export const getUserByID = (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id)
+    res.send(foundUser);
+}
+
+export const getUsers = (req, res) => {
+    res.send(users);
+}
+
+export const deleteUser = (req, res) => {
+    const { id } = req.params;
+
+    users = users.filter((user) => user.id !== id)
+
+    res.send(`User with the ID ${id} deleted from the database`);
+}
+
+export const updateUser = (req, res) => {
+    const { id } = req.params;
+    const { firstName, lastName, age } = req.body;
+    const user = users.find((user) => user.id === id);
+
+    if (firstName) user.firstName = firstName;
+    if (lastName) user.lastName = lastName;
+    if (age) user.age = age;
+    res.send(`user with ID ${id} has been updated in the database`);
+}
