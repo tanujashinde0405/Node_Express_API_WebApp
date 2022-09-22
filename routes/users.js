96ea6ad759db728767e33901a9fa00cc9df5763e
Tanuja Shinde <tanuja.shinde@seagate.com>
2022-09-22 10:07:37 +0530

    add get/post API's for users
    Signed-off-by: Tanuja Shinde <tanuja.shinde@seagate.com>

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,27 @@
 import express from 'express';
+import { v4 as uuidv4} from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        firstName: "Tanuja",
-        lastName: "Shinde",
-        age: 23
-    },
-    {
-        firstName: "Hrishikesh",
-        lastName: "Bawane",
-        age: 25
-    },
-]
+const users = []
 
 router.post('/', (req, res) => {
-    console.log("post request");
-    console.log("inserting an user..!")
-    res.send("Hello from post!!");
+    const user = req.body;
+    const userWithID = {...user, id: uuidv4()}
+    users.push(userWithID);
+    res.send(`User with the name ${user.firstName} added to the database`);
 })
 
 router.get('/', (req, res) => {
-    console.log(users);
-    res.send("Hello!!");
+    res.send(users);
 });
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    const foundUser = users.find((user) => user.id === id)
+    console.log(foundUser);
+    res.send(foundUser);
+})
 
 export default router;
